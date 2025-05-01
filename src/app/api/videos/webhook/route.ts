68a94f77e143b8068cec4d9e9b8dc61a77231331
Nirmal Kumar Gupta <nihalgupta.nirmal@gmail.com>
2025-05-01 15:27:54 +0530

    Updated video schema, handle video.asset.ready, asset.errored, asset.deleted and asset.track.ready webhook event

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -5,6 +5,7 @@ import {
     VideoAssetErroredWebhookEvent,
     VideoAssetReadyWebhookEvent,
     VideoAssetTrackReadyWebhookEvent,
+    VideoAssetDeletedWebhookEvent,
 } from "@mux/mux-node/resources/webhooks"
 
 import { mux } from "@/lib/mux";
@@ -17,7 +18,8 @@ type WebhookEvent =
     | VideoAssetCreatedWebhookEvent
     | VideoAssetReadyWebhookEvent
     | VideoAssetErroredWebhookEvent
-    | VideoAssetTrackReadyWebhookEvent;
+    | VideoAssetTrackReadyWebhookEvent
+    | VideoAssetDeletedWebhookEvent;
 
 export const POST = async(request: Request) => {
     if(!SIGNIN_SECRET) {
@@ -50,6 +52,9 @@ export const POST = async(request: Request) => {
                 return new Response("No upload ID found", { status: 400 });
             }
 
+            console.log("Creating video: ", { uploadId: data.upload_id });
+
+
             await db
                 .update(videos)
                 .set({
@@ -57,6 +62,93 @@ export const POST = async(request: Request) => {
                     muxStatus: data.status,
                 })
                 .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.ready": {
+            const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+            const playbackId = data.playback_ids?.[0].id;
+
+            if(!data.upload_id){
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            if(!playbackId){
+                return new Response("Missing playback ID", { status: 400 });
+            }
+
+            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+            const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+            const duration = data.duration ? Math.round(data.duration * 1000) : 0;
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                    muxPlaybackId: playbackId,
+                    muxAssetId: data.id,
+                    thumbnailUrl,
+                    previewUrl,
+                    duration,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.errored": {
+            const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+            if(!data.upload_id){
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.deleted": {
+            const data = payload.data as VideoAssetDeletedWebhookEvent["data"];
+
+            if(!data.upload_id){
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            console.log("Deleting video: ", { uploadId: data.upload_id });
+
+            await db
+                .delete(videos)
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.track.ready": {
+            const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+                asset_id: string;
+            }
+
+            console.log("Track Ready");
+
+            //Typescript incorrectly says that asset_id does not exist
+            const assetId = data.asset_id;
+            const trackId = data.id;
+            const status = data.status;
+
+            if(!assetId){
+                return new Response("Missing asset ID", { status: 404 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxTrackId: trackId,
+                    muxTrackStatus: status,
+                })
+                .where(eq(videos.muxAssetId, assetId));
             break;
         }
     }
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -7,6 +7,10 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import Link from "next/link";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideosSection = () => {
     return (
@@ -45,16 +49,40 @@ export const VideosSectionSuspense = () => {
                             <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                                 <TableRow className="cursor-pointer">
                                     <TableCell>
-                                        {video.title}
+                                        <div className="flex items-center gap-4">
+                                            <div className="relative aspect-video w-36 shrink-0">
+                                                <VideoThumbnail 
+                                                    imageUrl={video.thumbnailUrl} 
+                                                    previewUrl={video.previewUrl} 
+                                                    title={video.title}
+                                                    duration={video.duration || 0}
+                                                />
+                                            </div>
+                                            <div className="flex flex-col overflow-hidden gap-y-1">
+                                                <span className="text-sm line-clamp-1">{video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1">
+                                                    {video.description || "No Description"}
+                                                </span>
+                                            </div>
+                                        </div>
                                     </TableCell>
                                     <TableCell>
-                                        visibility
+                                        <div className="flex items-center">
+                                            {video.visibility === "private" ? (
+                                                <LockIcon className="size-4 mr-2" />
+                                            ) : (
+                                                <Globe2Icon className="size-4 mr-2" />
+                                            )}
+                                            {snakeCaseToTitle(video.visibility)}
+                                        </div>
                                     </TableCell>
                                     <TableCell>
-                                        status
+                                        <div className="flex items-center">
+                                            {snakeCaseToTitle(video.muxStatus || "error")}
+                                        </div>
                                     </TableCell>
-                                    <TableCell>
-                                        date
+                                    <TableCell className="text-sm truncate">
+                                        {format(new Date(video.createdAt), "d MMM yyyy")}
                                     </TableCell>
                                     <TableCell className="text-right">
                                         views
